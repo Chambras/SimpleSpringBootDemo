ee37ab0c9b46e9eba8fa6546cab176bc5fb80933
Marcelo Zambrana Villarroel <marcech.am@gmail.com>
2019-07-08 23:32:30 -0400

    initial commit

---
+++ src/main/java/com/demo/simplespringbootpage/SimplespringbootpageApplication.java
@@ -0,0 +1,13 @@
+package com.demo.simplespringbootpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplespringbootpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplespringbootpageApplication.class, args);
+    }
+
+}
